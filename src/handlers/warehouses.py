f205d011265baa9267ff2a2129963f9f7d151537
VEv <you@example.com>
2026-06-10 20:46:33 +0300

    [+] Handle is_central on edit

--- src/handlers/warehouses.py
+++ src/handlers/warehouses.py
@@ -65,16 +65,18 @@ def _render_warehouse(warehouse: Warehouse) -> None:
 
     console.print(panel)
 
+
 def _central_warehouse_exists() -> bool:
     conn = get_conn()
     with conn.cursor(row_factory=class_row(Warehouse)) as cur:
         cur.execute("SELECT * FROM catalog.warehouses WHERE is_central = true")
         warehouses: list[Warehouse] = cur.fetchall()
-        
+
     if len(warehouses) >= 1:
         return True
-    
+
     return False
+
 
 @command("list warehouses", "список всех складов", CATEGORY_WAREHOUSES)
 def list_warehouses() -> None:
@@ -154,13 +156,42 @@ def edit_warehouse(_id: str) -> None:
         "Адрес: ", default=warehouse.address, validator=NonEmptyValidator()
     ).strip()
     label = (
-        prompt("Метка (необязательно): ", default=warehouse.label or "").strip() or None
+            prompt("Метка (необязательно): ", default=warehouse.label or "").strip() or None
     )
-    conn.execute(
-        """UPDATE catalog.warehouses SET city = %s, address = %s, label = %s
-        WHERE id = %s""",
-        (city, address, label, _id),
-    )
+
+# TO DO check
+    set_is_central = False
+
+    if not warehouse.is_central:
+        set_is_central = prompt("Is central: ", default=str(warehouse.is_central)).strip()
+
+    if not warehouse.is_central and set_is_central:
+        old_id = 0
+        with conn.cursor(row_factory=class_row(Warehouse)) as cur:
+            cur.execute("SELECT * FROM catalog.warehouses WHERE is_central")
+            old_warehouse: Warehouse | None = cur.fetchone()
+
+            if old_warehouse is None:
+                return;
+
+            old_id = old_warehouse.id
+
+        conn.execute(
+            """UPDATE catalog.warehouses SET city = %s, address = %s, label = %s, is_central = %s
+            WHERE id = %s""",
+            (city, address, label, set_is_central, _id),
+        )
+
+        conn.execute(
+            """UPDATE catalog.warehouses SET is_central = false WHERE id = %s""", (old_id,)
+        )
+    else:
+        conn.execute(
+            """UPDATE catalog.warehouses SET city = %s, address = %s, label = %s
+            WHERE id = %s""",
+            (city, address, label, _id),
+        )
+
     if label:
         console.print(f"[green]Склад в городе {city} ({label}) обновлен [/green]")
     else:
