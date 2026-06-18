b37d1d8516e73603ad7913c6f4b7f8b546ca93e0
Evik4Erik <41995169+Evik4Erik@users.noreply.github.com>
2026-06-18 07:06:22 +0000

    Update up.sql

--- alembic/sql/15ff6ed40b8e/up.sql
+++ alembic/sql/15ff6ed40b8e/up.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA auth;
-CREATE TABLE auth.users(
+CREATE SCHEMA IF NOT EXISTS auth;
+CREATE TABLE IF NOT EXISTS  auth.users(
 	id SERIAL PRIMARY KEY,
 	username TEXT UNIQUE NOT NULL,
 	password TEXT NOT NULL,
@@ -11,5 +11,7 @@ GRANT SELECT ON TABLE auth.users to PUBLIC;
 
 insert into auth.users (username, password, role) VALUES ('cat_man', crypt('pass', gen_salt('bf')), 'catalog_manager');
 
-ALTER TABLE sales.orders ADD COLUMN created_by INT NOT NULL DEFAULT '1';
+ALTER TABLE sales.orders ADD COLUMN IF NOT EXISTS created_by INT NOT NULL DEFAULT '1';
+
+ALTER TABLE sales.orders DROP CONSTRAINT IF EXISTS created_by_user;
 ALTER TABLE sales.orders ADD CONSTRAINT created_by_user FOREIGN KEY (created_by) REFERENCES auth.users (id);
