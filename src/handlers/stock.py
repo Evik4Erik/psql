ca83fa5b7f96f91d7754630c1f460262205326f5
Evik4Erik <41995169+Evik4Erik@users.noreply.github.com>
2026-07-03 12:51:03 +0000

    Update stock.py

--- src/handlers/stock.py
+++ src/handlers/stock.py
@@ -101,8 +101,8 @@ def list_warehouse_stock() -> None:
 
     with conn.cursor(row_factory=class_row(Stock_w_view)) as cur:
         cur.execute("""
-                    WITH c_quantity AS (
-                        SELECT quantity AS common_quantity, product_id
+                    WITH avel AS (
+                        SELECT quantity AS available, product_id
                         FROM inventory.stocks  
                         WHERE warehouse_id = %s
                     ),
@@ -113,11 +113,11 @@ def list_warehouse_stock() -> None:
                     )
                     
                     SELECT p.id, p.name AS product_name, 
-                    COALESCE(common_quantity, 0) AS common_quantity,
+                    COALESCE(available, 0) AS available,
                     COALESCE(reserved, 0) AS reserved, 
-                    COALESCE(common_quantity, 0) - COALESCE(reserved, 0) AS available 
+                    COALESCE(available, 0) + COALESCE(reserved, 0) AS common_quantity 
                     FROM catalog.products p
-					LEFT JOIN c_quantity c ON p.id = c.product_id
+					LEFT JOIN avel a ON p.id = a.product_id
 					LEFT JOIN res r ON p.id = r.product_id
 					ORDER BY p.id """, (warehouse_id, warehouse_id))
         stocks: list[Stock_w_view] = cur.fetchall()
@@ -163,7 +163,7 @@ def list_warehouse_stock() -> None:
     table.add_column("available", style="magenta", min_width=6)
 
     with conn.cursor(row_factory=class_row(Stock_p_view)) as cur:
-        cur.execute("""WITH common_quantity AS 
+        cur.execute("""WITH available AS 
                             (
                                 SELECT quantity, warehouse_id 
                                 FROM inventory.stocks 
@@ -177,12 +177,12 @@ def list_warehouse_stock() -> None:
                             )
 
 
-                        SELECT w.id, cities.city, w.address, w.is_central, 
-                        COALESCE(c.quantity, 0) AS common_quantity,   
+                        SELECT w.id, cities.city, w.address, w.is_central,  
                         COALESCE(r.quantity, 0) AS reserved,
-                        COALESCE(c.quantity, 0) - COALESCE(r.quantity, 0) AS available
+                        COALESCE(a.quantity, 0) AS available,
+                        COALESCE(a.quantity, 0) + COALESCE(r.quantity, 0) AS common_quantity,  
                         FROM catalog.warehouses w
-                        LEFT JOIN common_quantity c ON w.id = c.warehouse_id
+                        LEFT JOIN available a ON w.id = a.warehouse_id
                         LEFT JOIN reserves r ON w.id = r.warehouse_id
                         LEFT JOIN catalog.cities cities ON w.city_id = cities.id
                         ORDER BY available DESC """, (product_id, product_id))
