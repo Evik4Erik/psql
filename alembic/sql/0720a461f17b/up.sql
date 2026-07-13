c3b9e9c9a9f5fc2ea521de0daf89d6ccf666b470
Evik4Erik <41995169+Evik4Erik@users.noreply.github.com>
2026-07-13 13:19:29 +0000

    Update up.sql

--- alembic/sql/0720a461f17b/up.sql
+++ alembic/sql/0720a461f17b/up.sql
@@ -1,1 +1,4 @@
 ALTER TABLE auth.users ADD COLUMN warehouse_id INT REFERENCES catalog.warehouses (id);
+
+CREATE OR REPLACE VIEW ship_delivery AS 
+SELECT id, status, warehouse_id FROM sales.orders;
